2f4b6a12b58ad7ff27a74f78d48cc2ca2d0b7703
Jack Koenig <koenig@sifive.com>
2018-11-09 11:43:50 -0600

    Try finding the package manually

--- install.sh
+++ install.sh
@@ -5,15 +5,12 @@ if [ ! -d chisel3 ]; then
   git clone --depth 1 git@github.com:freechipsproject/chisel3.git
 fi
 
-echo `pwd`
-ls
-ls ..
-ls ../..
-P=`./find.js firrtl-package`
-echo $P
-# Create symbolic link to firrtl
-#ln -fs $P
-
-#P2=`npm explore firrtl-package -- pwd`
-#ln -fs $P2
+if [ -d ../node_modules/firrtl-package ]; then
+  ln -s ../node_modules/firrtl-package
+elif [ -d node_modules/firrtl-package ]; then
+  ln -s node_modules/firrtl-package
+else
+  echo "ERROR: Couldn't find path to firrtl-package!"
+  exit 1
+fi
 
