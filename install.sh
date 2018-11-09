9e3326404c87d73f11b1c53a2c81712725bd6468
Jack Koenig <koenig@sifive.com>
2018-11-09 10:33:16 -0600

    Debugging...

--- install.sh
+++ install.sh
@@ -5,6 +5,10 @@ if [ ! -d chisel3 ]; then
   git clone --depth 1 git@github.com:freechipsproject/chisel3.git
 fi
 
+echo `pwd`
+ls
+ls ..
+ls ../..
 # Create symbolic link to firrtl
 ln -fs `./find.js firrtl-package`
 
